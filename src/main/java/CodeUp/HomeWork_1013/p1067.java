821570f3c11489413a14d8b1846b01cb7aee7fc1
Changbum97 <chb2005@naver.com>
2022-10-13 17:04:01 +0900

    CodeUp 10/13 과제 제출

---
+++ src/main/java/CodeUp/HomeWork_1013/p1067.java
@@ -0,0 +1,15 @@
+package CodeUp.HomeWork_1013;
+
+import java.util.Scanner;
+
+public class p1067 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        if(a >= 0) System.out.println("plus");
+        else System.out.println("minus");
+
+        if(a % 2 == 0) System.out.println("even");
+        else System.out.println("odd");
+    }
+}
