7de3ae692bfdccd415d00c3c0850b16b1f7894c4
Changbum97 <chb2005@naver.com>
2022-10-14 09:33:56 +0900

    Insertion Sort 클래스 생성

---
+++ src/main/java/Day8_1014/InsertionSort.java
@@ -0,0 +1,18 @@
+package Day8_1014;
+
+import java.util.Arrays;
+
+public class InsertionSort {
+    public static void main(String[] args) {
+        int[] arr = new int[]{8, 5, 6, 2, 4};
+        InsertionSort insertionSort = new InsertionSort();
+        int[] result = insertionSort.sort(arr);
+
+        System.out.println(Arrays.toString(result));
+    }
+
+    public int[] sort(int[] arr) {
+
+        return arr;
+    }
+}
