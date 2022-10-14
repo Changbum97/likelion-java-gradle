11fbb2a7fbec559f674b907e2cc5fe9ba2df11a7
Changbum97 <chb2005@naver.com>
2022-10-14 09:45:08 +0900

    삽입정렬 swap까지

--- src/main/java/Day8_1014/InsertionSort.java
+++ src/main/java/Day8_1014/InsertionSort.java
@@ -12,7 +12,12 @@ public class InsertionSort {
     }
 
     public int[] sort(int[] arr) {
-
+        int i = 1;
+        if(arr[i] < arr[i - 1]) {
+            int temp = arr[i];
+            arr[i] = arr[i - 1];
+            arr[i - 1] = temp;
+        }
         return arr;
     }
 }
