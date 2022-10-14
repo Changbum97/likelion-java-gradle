33bc5c8d148e4a75b0219e6fa80b6d8900f72bcc
Changbum97 <chb2005@naver.com>
2022-10-14 10:13:35 +0900

    코드업 1443(삽입 정렬)

--- src/main/java/CodeUp/p1443.java
+++ src/main/java/CodeUp/p1443.java
@@ -3,15 +3,8 @@ package CodeUp;
 import java.util.Scanner;
 
 public class p1443 {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int[] arr = new int[n];
 
-        for(int i = 0 ; i < n ; i ++) {
-            arr[i] = sc.nextInt();
-        }
-
+    static int[] sort(int[] arr, int n) {
         for(int i = 1 ; i < n ; i ++) {
             for(int j = i - 1 ; j >= 0 ; j --) {
                 if(arr[j + 1] < arr[j]) {
@@ -23,6 +16,18 @@ public class p1443 {
                 }
             }
         }
+        return arr;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+
+        for(int i = 0 ; i < n ; i ++) {
+            arr[i] = sc.nextInt();
+        }
+
+        arr = sort(arr, n);
 
         for(int i = 0 ; i < n ; i ++) {
             System.out.println(arr[i]);
