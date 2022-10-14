818badb781b453fb2afa0db8c8c267266e5ca733
Changbum97 <chb2005@naver.com>
2022-10-14 10:14:57 +0900

    삽입정렬 알고리즘

--- src/main/java/Day8_1014/InsertionSort.java
+++ src/main/java/Day8_1014/InsertionSort.java
@@ -12,11 +12,16 @@ public class InsertionSort {
     }
 
     public int[] sort(int[] arr) {
-        int i = 1;
-        if(arr[i] < arr[i - 1]) {
-            int temp = arr[i];
-            arr[i] = arr[i - 1];
-            arr[i - 1] = temp;
+        for(int i = 1 ; i < arr.length ; i ++) {
+            for(int j = i - 1 ; j >= 0 ; j --) {
+                if(arr[j + 1] < arr[j]) {
+                    int temp = arr[j + 1];
+                    arr[j + 1] = arr[j];
+                    arr[j] = temp;
+                } else {
+                    break;
+                }
+            }
         }
         return arr;
     }
