f42fbd6fb820688d221876d244c7f35a6016c3c1
Changbum97 <chb2005@naver.com>
2022-10-14 10:28:15 +0900

    삽입정렬 알고리즘 재귀적용

--- src/main/java/Day8_1014/InsertionSort.java
+++ src/main/java/Day8_1014/InsertionSort.java
@@ -6,23 +6,24 @@ public class InsertionSort {
     public static void main(String[] args) {
         int[] arr = new int[]{8, 5, 6, 2, 4};
         InsertionSort insertionSort = new InsertionSort();
-        int[] result = insertionSort.sort(arr);
+        int[] result = insertionSort.sort(arr, 1);
 
         System.out.println(Arrays.toString(result));
     }
 
-    public int[] sort(int[] arr) {
-        for(int i = 1 ; i < arr.length ; i ++) {
-            for(int j = i - 1 ; j >= 0 ; j --) {
-                if(arr[j + 1] < arr[j]) {
-                    int temp = arr[j + 1];
-                    arr[j + 1] = arr[j];
-                    arr[j] = temp;
-                } else {
-                    break;
-                }
+    public int[] sort(int[] arr, int i) {
+        if (i == arr.length) return arr;
+
+        for(int j = i - 1 ; j >= 0 ; j --) {
+            if(arr[j + 1] < arr[j]) {
+                int temp = arr[j + 1];
+                arr[j + 1] = arr[j];
+                arr[j] = temp;
+            } else {
+                break;
             }
         }
-        return arr;
+
+        return sort(arr, i + 1);
     }
 }
