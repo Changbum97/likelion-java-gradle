f806250ef16f4e871c4a7b8b6dcf251abe3d5863
Changbum97 <chb2005@naver.com>
2022-10-17 22:13:58 +0900

    CodeUp 10/17 과제 제출

---
+++ src/main/java/CodeUp/HomeWork_1017/p1083.java
@@ -0,0 +1,19 @@
+package CodeUp.HomeWork_1017;
+
+import java.util.Scanner;
+
+public class p1083 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        for(int i = 1 ; i <= n ; i ++) {
+            if(i % 3 == 0) {
+                System.out.print("X");
+            } else {
+                System.out.print(i);
+            }
+            System.out.print(" ");
+        }
+    }
+}
