5cd00838b337cdaddac8b8ac45a3a759c012045d
Changbum97 <chb2005@naver.com>
2022-10-17 12:23:11 +0900

    java, db 연동 select문 실습

--- src/main/java/Day9_1017/DBExercise/UserDao.java
+++ src/main/java/Day9_1017/DBExercise/UserDao.java
@@ -1,43 +1,75 @@
 package Day9_1017.DBExercise;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import javax.xml.namespace.QName;
+import java.sql.*;
 import java.util.Map;
 
 public class UserDao {
-    public void add() throws SQLException, ClassNotFoundException {
-        Map<String, String> env = System.getenv();
-        String dbHost = env.get("DB_HOST");
-        String dbUser = env.get("DB_USER");
-        String dbPassword = env.get("DB_PASSWORD");
 
-        Class.forName("com.mysql.cj.jdbc.Driver");
+    private Map<String, String> env;
+    private String dbHost, dbUser, dbPassword;
+    private Connection conn;
 
+    public UserDao() throws SQLException {
+        env = System.getenv();
+        dbHost = env.get("DB_HOST");
+        dbUser = env.get("DB_USER");
+        dbPassword = env.get("DB_PASSWORD");
         // DB 연결 (url, ID, PW)
-        Connection conn = DriverManager.getConnection(dbHost, dbUser, dbPassword);
+        conn = DriverManager.getConnection(dbHost, dbUser, dbPassword);
+    }
 
+    public void add() throws ClassNotFoundException, SQLException {
         // 쿼리 입력
         PreparedStatement ps = conn.prepareStatement(
                 "INSERT INTO users(id, name, password) values (?, ?, ?);"
         );
 
         // 쿼리에 파라미터 입력
-        ps.setString(1, "0");
+        ps.setString(1, "2");
         ps.setString(2, "Changbum");
         ps.setString(3, "1234");
 
-        // MySQL에 입력한 쿼리 실행
-        int status = ps.executeUpdate();
+        try {
+            // MySQL에 입력한 쿼리 실행
+            int status = ps.executeUpdate();
+            System.out.println("status : " + status);
+            System.out.println("DB Insert 완료");
+        } catch (SQLIntegrityConstraintViolationException e) {
+            System.out.println("id 중복");
+        }
 
         ps.close();
-        conn.close();
+    }
+
+    public void search() throws SQLException {
+        // 쿼리 입력
+        PreparedStatement ps = conn.prepareStatement(
+                "SELECT * FROM users WHERE id = ?;"
+        );
+
+        // 쿼리에 파라미터 입력
+        ps.setInt(1, 0);
+
+        // MySQL에 입력한 쿼리 실행
+        ResultSet resultSet = ps.executeQuery();
+        System.out.println("DB Search 완료");
+
+        while(resultSet.next()) {
+            System.out.printf("id : %s, name : %s, password : %s\n",
+                    resultSet.getString("id"),
+                    resultSet.getString("name"),
+                    resultSet.getString("password"));
+        }
+
+        ps.close();
     }
 
     // UserDao의 add 메소드 실행
     public static void main(String[] args) throws SQLException, ClassNotFoundException {
         UserDao userDao = new UserDao();
         userDao.add();
+        userDao.search();
+        userDao.conn.close();
     }
 }
