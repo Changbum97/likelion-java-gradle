61753ea7bcf4b11b8cca55e1edfba426e4039df4
Changbum97 <chb2005@naver.com>
2022-10-17 09:07:02 +0900

    별찍기 4단 직접 출력

---
+++ src/main/java/Day9_1017/RightTriangle.java
@@ -0,0 +1,16 @@
+package Day9_1017;
+
+public class RightTriangle {
+    /*
+    *
+    * *
+    * * *
+    * * * *
+     */
+    public static void main(String[] args) {
+        System.out.println("*");
+        System.out.println("* *");
+        System.out.println("* * *");
+        System.out.println("* * * *");
+    }
+}
