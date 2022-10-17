7aed2e3461dceaa27997e0dca41572b7b8ad1fab
Changbum97 <chb2005@naver.com>
2022-10-17 09:19:27 +0900

    직각삼각형 별찍기 반복문 사용

--- src/main/java/Day9_1017/RightTriangle.java
+++ src/main/java/Day9_1017/RightTriangle.java
@@ -1,14 +1,19 @@
 package Day9_1017;
 
 public class RightTriangle {
-    /*
-    *
-    * *
-    * * *
-    * * * *
-     */
+
     public static void main(String[] args) {
-        int n = 4;
+        RightTriangle rt = new RightTriangle();
+        rt.printRightTriangle(4);
+    }
+
+    /*
+     *
+     * *
+     * * *
+     * * * *
+     */
+    public void printRightTriangle(int n) {
         for(int i = 1 ; i <= n ; i ++) {
             for(int j = 1 ; j <= i ; j ++) {
                 System.out.print("* ");
