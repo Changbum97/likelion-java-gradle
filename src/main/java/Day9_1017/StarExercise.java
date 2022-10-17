680364f2bbf6a51b0932d2a8b256715bd61a783d
Changbum97 <chb2005@naver.com>
2022-10-17 10:16:24 +0900

    마름모 찍기

--- src/main/java/Day9_1017/StarExercise.java
+++ src/main/java/Day9_1017/StarExercise.java
@@ -10,7 +10,28 @@ public class StarExercise {
 
     public static void main(String[] args) {
         StarExercise rt = new StarExercise('*');
-        rt.printPyramid(7);
+        rt.printRhombus(5);
+    }
+
+    /*
+       *
+      ***
+     *****
+      ***
+       *
+     */
+    public void printRhombus(int n) {
+        printPyramid(n);
+        for(int i = n - 1 ; i >= 1 ; i --) {
+            for(int j = 1 ; j <= n - i ; j ++) {
+                System.out.print(" ");
+            }
+            for(int j = 1 ; j < 2 * i ; j ++) {
+                System.out.print(letter);
+            }
+            System.out.println();
+        }
+
     }
 
     /*
