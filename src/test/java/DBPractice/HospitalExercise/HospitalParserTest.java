afcbc4e11ab1d9282e8bbdb4b7313a103655641d
Changbum97 <chb2005@naver.com>
2022-10-18 11:05:19 +0900

    Hospital Test

--- src/test/java/DBPractice/HospitalExercise/HospitalParserTest.java
+++ src/test/java/DBPractice/HospitalExercise/HospitalParserTest.java
@@ -1,7 +1,7 @@
-package DBExercise.HospitalExercise;
+package DBPractice.HospitalExercise;
 
-import DBExercise.HospitalExercise.domain.Hospital;
-import DBExercise.HospitalExercise.parser.HospitalParser;
+import DBPractice.HospitalExercise.domain.Hospital;
+import DBPractice.HospitalExercise.parser.HospitalParser;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
