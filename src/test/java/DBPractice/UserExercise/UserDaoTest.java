c79fd3823b12c365ce9d81a1da9460d82b2ba70a
Changbum97 <chb2005@naver.com>
2022-10-18 11:28:54 +0900

    Add and Select Test 추가

--- src/test/java/DBPractice/UserExercise/UserDaoTest.java
+++ src/test/java/DBPractice/UserExercise/UserDaoTest.java
@@ -1,4 +1,20 @@
+package DBPractice.UserExercise;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.sql.SQLException;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class UserDaoTest {
-  
+    @Test
+    void addAndSelect() throws SQLException, ClassNotFoundException {
+        UserDao userDao = new UserDao();
+        User user = new User("9", "EternityHwan", "1234");
+        userDao.add(user);
+
+        User selectedUser = userDao.findById("9");
+        Assertions.assertEquals("EternityHwan", selectedUser.getName());
+    }
 }
