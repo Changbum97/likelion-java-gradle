4aca39f5f8ce9c13fa5a4967c4c0330a3cffb708
Changbum97 <chb2005@naver.com>
2022-10-19 13:06:49 +0900

    User Dao Factory 생성 및 테스트

--- src/main/java/DBPractice/UserExercise/dao/UserDaoFactory.java
+++ src/main/java/DBPractice/UserExercise/dao/UserDaoFactory.java
@@ -1,7 +1,8 @@
 package DBPractice.UserExercise.dao;
 
+// 팩토리에서 조립하는 역할 담당
 public class UserDaoFactory {
-    // 조립
+
     public UserDao localUserDao() {
         UserDao userDao = new UserDao(new LocalConnectionMaker());
         return userDao;
