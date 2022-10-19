054909a77e77b3ef8ca35858577e8fa2bc89950f
Changbum97 <chb2005@naver.com>
2022-10-19 10:21:01 +0900

    Stack 구현 및 테스트 코드 작성

--- src/main/java/Week3/Day11_1019/Stack01.java
+++ src/main/java/Week3/Day11_1019/Stack01.java
@@ -1,2 +1,20 @@
-package Week3.Day11_1019;public class Stack01 {
+package Week3.Day11_1019;
+
+public class Stack01 {
+
+    private int[] arr = new int[10000];
+    private int pointer = 0;
+    public Stack01() { }
+
+    public Stack01(int size) {
+        arr = new int[size];
+    }
+
+    public void push(int value) {
+        arr[pointer ++] = value;
+    }
+
+    public int[] getArr() {
+        return arr;
+    }
 }
--- src/test/java/Week3/Day11_1019/Stack01Test.java
+++ src/test/java/Week3/Day11_1019/Stack01Test.java
@@ -1,4 +1,23 @@
+package Week3.Day11_1019;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class Stack01Test {
-  
+
+    @Test
+    @DisplayName("Stack Push Test")
+    void pushTest() {
+        Stack01 stack01 = new Stack01(100);
+        stack01.push(10);
+        stack01.push(20);
+
+        int[] arr = stack01.getArr();
+        Assertions.assertEquals(10, arr[0]);
+        Assertions.assertEquals(20, arr[1]);
+    }
+
 }
