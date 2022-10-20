6261e417348d6bb306bf84e19c4bcce3eb71e11f
Changbum97 <chb2005@naver.com>
2022-10-20 09:29:26 +0900

    Stack02 생성 및 테스트 코드 작성

--- src/main/java/Week3/Day12_1020/Stack02.java
+++ src/main/java/Week3/Day12_1020/Stack02.java
@@ -1,2 +1,25 @@
-package Week3.Day12_1020;public class Stack02 {
+package Week3.Day12_1020;
+
+public class Stack02 {
+
+    private int[] arr = new int[10000];
+    private int pointer = 0;
+    public Stack02() { }
+
+    public Stack02(int size) {
+        arr = new int[size];
+    }
+
+    public void push(int value) {
+        arr[pointer ++] = value;
+    }
+
+    public int pop() {
+        return arr[-- pointer];
+    }
+
+    public int[] getArr() {
+        return arr;
+    }
+
 }
--- src/test/java/Week3/Day12_1020/Stack02Test.java
+++ src/test/java/Week3/Day12_1020/Stack02Test.java
@@ -1,4 +1,41 @@
+package Week3.Day12_1020;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class Stack02Test {
-  
+    @BeforeEach
+    void beforeEach() {
+        System.out.println("before Each");
+    }
+
+    @Test
+    @DisplayName("Stack Push Test")
+    void pushTest() {
+        Stack02 stack02 = new Stack02(100);
+        stack02.push(10);
+        stack02.push(20);
+        stack02.push(30);
+
+        int arr[] = stack02.getArr();
+        assertEquals(10, arr[0]);
+        assertEquals(20, arr[1]);
+        assertEquals(30, arr[2]);
+    }
+
+    @Test
+    @DisplayName("Stack Push And Pop Test")
+    void pushAndPopTest() {
+        Stack02 stack02 = new Stack02(100);
+        stack02.push(10);
+        stack02.push(20);
+        assertEquals(20, stack02.pop());
+
+        stack02.push(30);
+        assertEquals(30, stack02.pop());
+        assertEquals(10, stack02.pop());
+    }
 }
