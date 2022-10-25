070de7a460f7dc991ec20d5c9be40850bd6351f5
Changbum97 <chb2005@naver.com>
2022-10-25 10:08:00 +0900

    Hash 실습

---
+++ src/main/java/Week4/Day15_1025/HashFunction.java
@@ -0,0 +1,17 @@
+package Week4.Day15_1025;
+
+public class HashFunction {
+    public int hash(String key) {
+        int asciiSum = 0;
+        for(int i = 0 ; i < key.length() ; i ++) {
+            asciiSum += key.charAt(i);
+        }
+        return asciiSum % 10000;
+    }
+
+    public static void main(String[] args) {
+        HashFunction hf = new HashFunction();
+        int result = hf.hash("kyeongrok");
+        System.out.println(result);
+    }
+}
---
+++ src/main/java/Week4/Day15_1025/HashTable.java
@@ -0,0 +1,56 @@
+package Week4.Day15_1025;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class HashTable {
+
+    private int size = 10000;
+    private int[] table = new int[size];
+
+    public HashTable() { }
+
+    public HashTable(int size) {
+        this.size = size;
+        this.table = new int[size];
+    }
+
+    public int hash(String key) {
+        int asciiSum = 0;
+        for(int i = 0 ; i < key.length() ; i ++) {
+            asciiSum += key.charAt(i);
+        }
+        return asciiSum % size;
+    }
+
+    public void insert(String key, int value) {
+        int hashCode = hash(key);
+        this.table[hashCode] = value;
+        System.out.println(key + " : " + hashCode + "방에 저장이 완료되었습니다.");
+    }
+
+    public int search(String key) {
+        return this.table[hash(key)];
+    }
+
+    public static void main(String[] args) {
+        // 학생 이름 80개
+        String[] names = new String[]{"DongyeonKang", "SubinKang", "KwanwunKo", "HyunseokKo", "KyoungdukKoo", "YeonjiGu", "SoyeonKown", "OhsukKwon", "GunwooKim", "KiheonKim", "NayeongKim", "DohyeonKim", "MinkyoungKim", "MinjiKim", "SanghoKim", "SolbaeKim", "YejinKim", "EungjunKim", "JaegeunKim", "JeonghyeonKim", "JunhoKim", "JisuKim", "kimjinah", "HaneulKim", "HeejungKim", "KimoonPark", "EunbinPark", "JeongHoonPark", "JeminPark", "TaegeunPark", "JiwonBae", "SeunggeunBaek", "JihwanByeon", "HeungseopByeon", "JeongHeeSeo", "TaegeonSeo", "SeeYunSeok", "SuyeonSeong", "SeyoelSon", "MinjiSong", "JinwooSong", "hyunboSim", "SominAhn", "JiyoungAhn", "ChangbumAn", "SoonminEom", "HyeongsangOh", "SuinWoo", "JuwanWoo", "InkyuYoon", "GahyunLee", "DaonLee", "DohyunLee", "SanghunLee", "SujinLee", "AjinLee", "YeonJae", "HyeonjuLee", "HakjunYim", "SeoyunJang", "SeohyeonJang", "JinseonJang", "SujinJeon", "SeunghwanJeon", "DaehwanJung", "JaeHyunJeung", "HeejunJeong", "GukhyeonCho", "MunjuJo", "YejiJo", "ChanminJu", "MinjunChoi", "SujeongChoi", "SeunghoChoi", "AyeongChoi", "GeonjooHan", "JinhyuckHeo", "MinwooHwang", "SieunHwang", "JunhaHwang"};
+
+        HashTable ht = new HashTable(200);
+        Set<Integer> nameSet = new HashSet<>();
+        for(int i = 0 ; i < names.length ; i ++) {
+            nameSet.add(ht.hash(names[i]));
+        }
+
+        System.out.printf("%d %d\n", names.length, nameSet.size());
+
+        for(int i = 0 ; i < names.length ; i ++) {
+            ht.insert(names[i], ht.hash(names[i]));
+        }
+
+        System.out.println(ht.search("ChangbumAn"));
+
+    }
+
+}
