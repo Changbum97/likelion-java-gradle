d0e9ad2d36ee2465c2a2316236c56b7a0ce52b68
Changbum97 <chb2005@naver.com>
2022-10-25 10:29:38 +0900

    프로그래머스 올바른 괄호

--- src/main/java/Programmers/bracket/CorrectBracket.java
+++ src/main/java/Programmers/bracket/CorrectBracket.java
@@ -1,2 +1,42 @@
-package Programmers.bracket;public class CorrectBracket {
+package Programmers.bracket;
+
+
+import Programmers.digit.AddAllDigit;
+
+import java.util.Scanner;
+
+/**
+ * 프로그래머스 올바른 괄호
+ */
+public class CorrectBracket {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Solution solution = new Solution();
+        System.out.println( solution.solution(sc.nextLine()) );
+    }
+
+    static class Solution {
+        boolean solution(String s) {
+            boolean answer = true;
+
+            int open = 0;
+            for(int i = 0 ; i < s.length() ; i ++) {
+                if(s.charAt(i) == '(') {
+                    open ++;
+                } else {
+                    open --;
+                    if(open == -1) {
+                        answer = false;
+                        break;
+                    }
+                }
+            }
+            if(open != 0) {
+                answer = false;
+            }
+
+            return answer;
+        }
+    }
 }
