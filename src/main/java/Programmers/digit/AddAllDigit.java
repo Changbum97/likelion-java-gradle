d8d1a127cdbe126e52e989a51716b2b3873ce7dd
Changbum97 <chb2005@naver.com>
2022-10-25 10:17:29 +0900

    프로그래머스 자릿수 더하기

--- src/main/java/Programmers/digit/AddAllDigit.java
+++ src/main/java/Programmers/digit/AddAllDigit.java
@@ -2,16 +2,19 @@ package Programmers.digit;
 
 import java.util.Scanner;
 
+/**
+ * 프로그래머스 : 자릿수 더하기
+ */
 public class AddAllDigit {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
-        System.out.println( );
-
+        int n = sc.nextInt();
+        Solution solution = new Solution();
+        System.out.println( solution.solution(n) );
     }
 
-    class Solution {
+    static class Solution {
         public int solution(int n) {
             int sum = 0;
 
---
+++ src/main/java/Programmers/hash/NotCompletionPlayer.java
@@ -0,0 +1,2 @@
+package Programmers.hash;public class NotCompletionPlayer {
+}
