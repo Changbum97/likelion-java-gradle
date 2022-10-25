8b6a1a95cab537ff641107a55041913161e85c63
Changbum97 <chb2005@naver.com>
2022-10-25 10:08:55 +0900

    programmers

--- src/main/java/Programmers/digit/AddAllDigit.java
+++ src/main/java/Programmers/digit/AddAllDigit.java
@@ -1,2 +1,25 @@
-package Programmers.digit;public class AddAllDigit {
+package Programmers.digit;
+
+import java.util.Scanner;
+
+public class AddAllDigit {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println( );
+
+    }
+
+    class Solution {
+        public int solution(int n) {
+            int sum = 0;
+
+            while(n != 0) {
+                sum += n % 10;
+                n /= 10;
+            }
+            return sum;
+        }
+    }
 }
