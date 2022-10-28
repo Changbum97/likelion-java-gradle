523505f1216dd69ba79920d7b6dbfe3fbc25f97d
Changbum97 <chb2005@naver.com>
2022-10-28 09:18:01 +0900

    프로그래머스 폰켓몬 Hash Set 사용

--- src/main/java/Programmers/hash/Ponketmon.java
+++ src/main/java/Programmers/hash/Ponketmon.java
@@ -17,15 +17,13 @@ public class Ponketmon {
     }
     static class Solution {
         public int solution(int[] nums) {
-            int answer = 0;
             Set<Integer> set = new HashSet<>();
 
             for(int num : nums) {
                 set.add(num);
             }
 
-            answer = Math.min(set.size(), nums.length/2);
-            return answer;
+            return Math.min(set.size(), nums.length/2);
         }
     }
 }
