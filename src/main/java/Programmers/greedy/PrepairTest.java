11dcc1d52c16e3c97966b920e98425e0ca75fe0c
Changbum97 <chb2005@naver.com>
2022-10-31 09:43:23 +0900

    프로그래머스 모의고사 푸는중

--- src/main/java/Programmers/greedy/PrepairTest.java
+++ src/main/java/Programmers/greedy/PrepairTest.java
@@ -1,11 +1,13 @@
 package Programmers.greedy;
 
+import java.util.Arrays;
+
 public class PrepairTest {
     public static void main(String[] args) {
         Solution s = new Solution();
 
-        System.out.println(s.solution(new int[]{1, 2, 3, 4, 5}));
-        System.out.println(s.solution(new int[]{1, 3, 2, 4, 2}));
+        System.out.println(Arrays.toString( s.solution(new int[]{1, 2, 3, 4, 5, 1, 2, 3}) ));
+        System.out.println(Arrays.toString( s.solution(new int[]{1, 3, 2, 4, 2}) ));
     }
 
     static class Solution {
@@ -19,18 +21,29 @@ public class PrepairTest {
             int[] brr = new int[]{2, 1, 2, 3, 2, 4, 2, 5};
             int[] crr = new int[]{3, 3, 1, 1, 2, 2, 4, 4, 5 ,6};
 
-            int aIdx = 0, bIdx = 0, cIdx = 0;
             int aCnt = 0, bCnt = 0, cCnt = 0;
 
             for(int i = 0 ; i < answers.length ; i ++) {
-                if(answers[i] == arr[aIdx]) aCnt ++;
-                if(answers[i] == brr[bIdx]) bCnt ++;
-                if(answers[i] == crr[cIdx]) cCnt ++;
-
-                aIdx ++; bIdx ++; cIdx ++;
-
-
+                if(answers[i] == arr[i % arr.length]) aCnt ++;
+                if(answers[i] == brr[i % brr.length]) bCnt ++;
+                if(answers[i] == crr[i % crr.length]) cCnt ++;
             }
+
+            int max = Math.max(aCnt, bCnt);
+            max = Math.max(max, cCnt);
+
+            int ansSize = 0;
+            if(max == aCnt) ansSize ++;
+            if(max == bCnt) ansSize ++;
+            if(max == cCnt) ansSize ++;
+
+            int[] answer = new int[ansSize];
+            int ansIdx = 0;
+            if(max == aCnt) answer[ansIdx ++] = 1;
+            if(max == bCnt) answer[ansIdx ++] = 2;
+            if(max == cCnt) answer[ansIdx ++] = 3;
+
+            return answer;
         }
     }
 }
