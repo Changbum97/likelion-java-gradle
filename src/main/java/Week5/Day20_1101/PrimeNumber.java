34048a5a53fc066948a5a8d726b6a8d4156da966
Changbum97 <chb2005@naver.com>
2022-11-01 10:18:47 +0900

    Template Callback 적용 1단계

--- src/main/java/Week5/Day20_1101/PrimeNumber.java
+++ src/main/java/Week5/Day20_1101/PrimeNumber.java
@@ -2,20 +2,24 @@ package Week5.Day20_1101;
 
 public class PrimeNumber {
     public static void main(String[] args) {
+        PrimeNumber primeNumber = new PrimeNumber();
         // 13, 17, 19, 23이 소수 인지 판별
-        System.out.println(isPrimeNumber(13));
-        System.out.println(isPrimeNumber(16));
-        System.out.println(isPrimeNumber(19));
-        System.out.println(isPrimeNumber(23));
+        System.out.println(primeNumber.isPrimeNumber(13));
+        System.out.println(primeNumber.isPrimeNumber(16));
+        System.out.println(primeNumber.isPrimeNumber(19));
+        System.out.println(primeNumber.isPrimeNumber(23));
     }
 
-    static boolean isPrimeNumber(int x) {
-        int end = (int)Math.sqrt(x);
-        for(int i = 2 ; i <= end ; i ++) {
+    boolean isPrimeNumber(int x) {
+        for(int i = 2 ; someOperation(i, x) ; i ++) {
             if(x % i == 0) {
                 return false;
             }
         }
         return true;
+    }
+
+    boolean someOperation(int a, int b) {
+        return a < b;
     }
 }
