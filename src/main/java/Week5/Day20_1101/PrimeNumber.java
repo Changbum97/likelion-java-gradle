06495abb48041863a63191ae30350a11cc60bf42
Changbum97 <chb2005@naver.com>
2022-11-01 10:46:11 +0900

    소수 구하기 Template Callback lambda, 익명 클래스 적용

--- src/main/java/Week5/Day20_1101/PrimeNumber.java
+++ src/main/java/Week5/Day20_1101/PrimeNumber.java
@@ -7,23 +7,29 @@ interface StatementStrategy {
 public class PrimeNumber {
     public static void main(String[] args) {
         PrimeNumber primeNumber = new PrimeNumber();
-        // 13, 17, 19, 23이 소수 인지 판별
-        System.out.println(primeNumber.isPrimeNumber(13));
-        System.out.println(primeNumber.isPrimeNumber(16));
-        System.out.println(primeNumber.isPrimeNumber(19));
-        System.out.println(primeNumber.isPrimeNumber(23));
+        // 13, 17, 19, 23이 소수 인지 판별 => Template Callback 적용
+
+        // lambda 적용, 소수를 구하는 3가지 방법 해보기
+        System.out.println(primeNumber.isPrimeNumber(13, (a, b) -> a < b));
+        System.out.println(primeNumber.isPrimeNumber(17, (a, b) -> a * 2 < b));
+        System.out.println(primeNumber.isPrimeNumber(19, (a, b) -> a * a < b));
+
+        // 익명 클래스 적용
+        System.out.println(primeNumber.isPrimeNumber(23, new StatementStrategy() {
+            @Override
+            public boolean compare(int a, int b) {
+                return a * a < b;
+            }
+        }));
     }
 
     boolean isPrimeNumber(int num, StatementStrategy stmt) {
         for(int i = 2 ; stmt.compare(i, num) ; i ++) {
+            System.out.println(num + " " + i);
             if(num % i == 0) {
                 return false;
             }
         }
         return true;
-    }
-
-    boolean someOperation(int a, int b) {
-        return a < b;
     }
 }
