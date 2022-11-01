148089b823a67a77401f84cd442b1e76b8b4c46e
Changbum97 <chb2005@naver.com>
2022-11-01 09:34:39 +0900

    소수 판별하기 3번 방법

--- src/main/java/Week5/Day20_1101/PrimeNumber.java
+++ src/main/java/Week5/Day20_1101/PrimeNumber.java
@@ -4,14 +4,14 @@ public class PrimeNumber {
     public static void main(String[] args) {
         // 13, 17, 19, 23이 소수 인지 판별
         System.out.println(isPrimeNumber(13));
-        System.out.println(isPrimeNumber(17));
+        System.out.println(isPrimeNumber(16));
         System.out.println(isPrimeNumber(19));
         System.out.println(isPrimeNumber(23));
     }
 
     static boolean isPrimeNumber(int x) {
-        // 1번 방법
-        for(int i = 2 ; i < x ; i ++) {
+        int end = (int)Math.sqrt(x);
+        for(int i = 2 ; i <= end ; i ++) {
             if(x % i == 0) {
                 return false;
             }
