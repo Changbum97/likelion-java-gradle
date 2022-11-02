720366179831484eabe6273e514a6000ab524400
Changbum97 <chb2005@naver.com>
2022-11-02 09:42:23 +0900

    에라토스테네스의 체 1단계

--- src/main/java/Week5/Day21_1102/RemoveMultipleOf.java
+++ src/main/java/Week5/Day21_1102/RemoveMultipleOf.java
@@ -1,35 +1,38 @@
 package Week5.Day21_1102;
 
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.List;
 
-/**
- * 프로그래머스 소수 찾기
- */
+
 // 에라토스테네스의 체
 public class RemoveMultipleOf {
 
     public static void main(String[] args) {
         Solution s = new Solution();
-        System.out.println(s.solution(10));
-        System.out.println(s.solution(5));
-        System.out.println(s.solution(50));
+        s.solution(50);
     }
 
     static class Solution {
         public int solution(int n) {
             int answer = 0;
 
-            boolean[] isPrime = new boolean[n + 1];
-            Arrays.fill(isPrime, true);
-
+            List<Integer> list = new ArrayList<>();
             for(int i = 2 ; i <= n ; i ++) {
-                if(isPrime[i] == false) continue;
+                list.add(i);
+            }
 
-                answer ++;
-                for(int j = i * 2 ; j <= n ; j += i) {
-                    isPrime[j] = false;
+            for(int i = 0 ; i < list.size() ; i ++) {
+                if(list.get(i) % 2 == 0) {
+                    list.remove(i);
                 }
             }
+
+            System.out.println("2의 배수 지우고 남은 개수 : " + list.size());
+            System.out.print("남은 수 : ");
+            for(int num : list) {
+                System.out.print(num + " ");
+            }
+            System.out.println();
 
             return answer;
         }
