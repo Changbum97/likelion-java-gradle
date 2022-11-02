5f53d2d4fcef84cdb5ed230b779a23525808d6e0
Changbum97 <chb2005@naver.com>
2022-11-02 09:36:32 +0900

    프로그래머스 소수 찾기 - 에라토스테네스의 체 활용

--- src/main/java/Week5/Day21_1102/RemoveMultipleOf.java
+++ src/main/java/Week5/Day21_1102/RemoveMultipleOf.java
@@ -1,35 +1,34 @@
 package Week5.Day21_1102;
 
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
 
+/**
+ * 프로그래머스 소수 찾기
+ */
 // 에라토스테네스의 체
 public class RemoveMultipleOf {
 
     public static void main(String[] args) {
         Solution s = new Solution();
-        s.solution(50);
+        System.out.println(s.solution(10));
+        System.out.println(s.solution(5));
+        System.out.println(s.solution(50));
     }
 
     static class Solution {
         public int solution(int n) {
             int answer = 0;
 
-            List<Integer> list = new ArrayList<>();
+            boolean[] isPrime = new boolean[n + 1];
+            Arrays.fill(isPrime, true);
+
             for(int i = 2 ; i <= n ; i ++) {
-                list.add(i);
-            }
+                if(isPrime[i] == false) continue;
 
-            for(int i = 0 ; i < list.size() ; i ++) {
-                if(list.get(i) % 2 == 0) {
-                    list.remove(i);
+                answer ++;
+                for(int j = i * 2 ; j <= n ; j += i) {
+                    isPrime[j] = false;
                 }
-            }
-
-            System.out.println(list.size());
-            for(int num : list) {
-                System.out.print(num + " ");
             }
 
             return answer;
