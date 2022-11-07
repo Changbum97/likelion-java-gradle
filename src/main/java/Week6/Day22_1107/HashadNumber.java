68ee1d387791f43b261b71beb223a4a62dce5b49
Changbum97 <chb2005@naver.com>
2022-11-07 09:10:30 +0900

    하샤드 수 시작

---
+++ src/main/java/Week6/Day22_1107/HashadNumber.java
@@ -0,0 +1,15 @@
+package Week6.Day22_1107;
+
+public class HashadNumber {
+
+    public static void main(String[] args) {
+
+    }
+
+    static class Solution{
+        public boolean solution(int x) {
+            boolean answer = true;
+            return true;
+        }
+    }
+}
