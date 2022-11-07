92526cdf554f3eba4c9756453d874e49e2955575
Changbum97 <chb2005@naver.com>
2022-11-07 09:15:28 +0900

    프로그래머스 하샤드 수 통과

--- src/main/java/Week6/Day22_1107/HashadNumber.java
+++ src/main/java/Week6/Day22_1107/HashadNumber.java
@@ -3,13 +3,27 @@ package Week6.Day22_1107;
 public class HashadNumber {
 
     public static void main(String[] args) {
-
+        Solution s = new Solution();
+        System.out.println(s.solution(10));
+        System.out.println(s.solution(11));
+        System.out.println(s.solution(12));
+        System.out.println(s.solution(13));
     }
 
     static class Solution{
         public boolean solution(int x) {
-            boolean answer = true;
-            return true;
+            int sum = 0;
+
+            int temp = x;
+            while(temp != 0) {
+                sum += temp % 10;
+                temp /= 10;
+            }
+
+            if(x % sum == 0) {
+                return true;
+            }
+            return false;
         }
     }
 }
