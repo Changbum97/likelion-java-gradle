bfedd4f0b9768262f19d42f359b603de093e4779
Changbum97 <chb2005@naver.com>
2022-11-08 10:08:05 +0900

    이진탐색 구현

--- src/main/java/Week6/Day23_1108/BinarySearch.java
+++ src/main/java/Week6/Day23_1108/BinarySearch.java
@@ -1,2 +1,21 @@
-package Week6.Day23_1108;public class BinarySearch {
+package Week6.Day23_1108;
+
+public class BinarySearch {
+
+    public static void main(String[] args) {
+        int[] arr = new int[]{1, 3, 5, 6, 7, 9, 10, 15, 20};
+
+        int idx = binarySearch(0, arr.length, 7, arr);
+        System.out.println(idx);
+    }
+
+    static int binarySearch(int left, int right, int findNum, int[] arr) {
+        int mid = (left + right) / 2;
+        if(arr[mid] == findNum) return mid;
+        else if(arr[mid] > findNum) {
+            return binarySearch(left, mid - 1, findNum, arr);
+        } else {
+            return binarySearch(mid + 1, right, findNum, arr);
+        }
+    }
 }
