c7a99299cab414bd06727b0153886fea1eaf600e
Changbum97 <chb2005@naver.com>
2022-11-09 10:49:50 +0900

    BiFunction을 사용한 선택정렬

---
+++ src/main/java/Week6/Day24_1109/SelectionSort_UseFunction.java
@@ -0,0 +1,44 @@
+package Week6.Day24_1109;
+
+import java.util.Arrays;
+import java.util.function.BiFunction;
+
+public class SelectionSort_UseFunction {
+
+    public static void main(String[] args) {
+        int[] arr = new int[]{2, 7, 4, 9, 10, 223, 111, 23, 3, 39};
+
+        // 오름차순 정렬
+        int[] asc = selectionSort(arr.clone(), (a, b) -> a > b);
+
+        // 내림차순 정렬
+        int[] desc = selectionSort(arr.clone(), (a, b) -> a < b);
+
+        System.out.println("입력배열 : " + Arrays.toString(arr));
+        System.out.println("오름차순 : " + Arrays.toString(asc));
+        System.out.println("내림차순 : " + Arrays.toString(desc));
+
+    }
+
+    static int[] selectionSort(int[] arr, BiFunction<Integer, Integer, Boolean> stmt) {
+
+        int n = arr.length;
+        for(int i = 0 ; i < n - 1; i ++) {
+            int idx = i;
+
+            for(int j = i + 1 ; j < n ; j ++) {
+                if(stmt.apply(arr[idx], arr[j])) {
+                    idx = j;
+                }
+            }
+
+            if(i != idx) {
+                int temp = arr[i];
+                arr[i] = arr[idx];
+                arr[idx] = temp;
+            }
+        }
+
+        return arr;
+    }
+}
