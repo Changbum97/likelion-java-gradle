05cf074e6311a1bdda35cab31be8d4402d61ece0
Changbum97 <chb2005@naver.com>
2022-11-09 09:13:38 +0900

    선택 정렬

---
+++ src/main/java/Week6/Day24_1109/SelectionSort.java
@@ -0,0 +1,32 @@
+package Week6.Day24_1109;
+
+import java.util.Arrays;
+
+public class SelectionSort {
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(selectionSort(new int[]{2, 7, 4, 9, 10, 223, 111, 23, 3, 39})));
+    }
+
+    static int[] selectionSort(int[] arr) {
+        int n = arr.length;
+        for(int i = 0 ; i < n - 1; i ++) {
+            int min = arr[i];
+            int idx = i;
+
+            for(int j = i + 1 ; j < n ; j ++) {
+                if(min > arr[j]) {
+                    min = arr[j];
+                    idx = j;
+                }
+            }
+
+            if(i != idx) {
+                int temp = arr[i];
+                arr[i] = arr[idx];
+                arr[idx] = temp;
+            }
+        }
+
+        return arr;
+    }
+}
