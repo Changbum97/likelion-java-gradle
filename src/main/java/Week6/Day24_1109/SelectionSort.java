b43f715b78ea889d43a878132f9733c222b0dc34
Changbum97 <chb2005@naver.com>
2022-11-09 10:03:54 +0900

    Selection Sort interface, lambda 적용

--- src/main/java/Week6/Day24_1109/SelectionSort.java
+++ src/main/java/Week6/Day24_1109/SelectionSort.java
@@ -2,30 +2,39 @@ package Week6.Day24_1109;
 
 import java.util.Arrays;
 
+interface SortStrategy {
+    boolean compare(int a, int b);
+}
+
 public class SelectionSort {
     public static void main(String[] args) {
-        System.out.println(Arrays.toString(selectionSort(new int[]{2, 7, 4, 9, 10, 223, 111, 23, 3, 39})));
+        int[] arr = new int[]{2, 7, 4, 9, 10, 223, 111, 23, 3, 39};
+
+        // 오름차순 정렬
+        int[] asc = selectionSort(arr, (a, b) -> a > b);
+        System.out.println("오름차순 : " + Arrays.toString(asc));
+
+        // 내림차순 정렬
+        int[] desc = selectionSort(arr, (a, b) -> a < b);
+        System.out.println("내림차순 : " + Arrays.toString(desc));
+
     }
 
-    static int[] selectionSort(int[] arr) {
+    static int[] selectionSort(int[] arr, SortStrategy stmt) {
         int n = arr.length;
         for(int i = 0 ; i < n - 1; i ++) {
-            int min = arr[i];
-            int minIdx = i;
+            int idx = i;
 
             for(int j = i + 1 ; j < n ; j ++) {
-                if(min > arr[j]) {
-                    min = arr[j];
-                    minIdx = j;
+                if(stmt.compare(arr[idx], arr[j])) {
+                    idx = j;
                 }
             }
 
-            System.out.println(i + " : " + minIdx);
-
-            if(i != minIdx) {
+            if(i != idx) {
                 int temp = arr[i];
-                arr[i] = arr[minIdx];
-                arr[minIdx] = temp;
+                arr[i] = arr[idx];
+                arr[idx] = temp;
             }
         }
 
