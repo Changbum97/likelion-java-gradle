3cf199a6bab6f7ba6304237ac032964f3388b277
Changbum97 <chb2005@naver.com>
2022-11-10 09:33:38 +0900

    프로그래머스 같은 숫자는 싫어 완료 - 스택 사용

--- src/main/java/Programmers/stack_queue/notsamenumber_stack.java
+++ src/main/java/Programmers/stack_queue/notsamenumber_stack.java
@@ -1,2 +1,32 @@
-package Programmers.stack_queue;public class notsamenumber_stack {
+package Programmers.stack_queue;
+
+import java.util.Arrays;
+import java.util.Stack;
+
+public class notsamenumber_stack {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        System.out.println(Arrays.toString(s.solution(new int[]{1, 1, 3, 3, 0, 1, 1})));
+        System.out.println(Arrays.toString(s.solution(new int[]{4, 4, 4, 3, 3})));
+    }
+
+    static class Solution {
+        public int[] solution(int[] arr) {
+            Stack<Integer> answerStack = new Stack<>();
+            answerStack.push(arr[0]);
+
+            for(int i = 1 ; i < arr.length ; i ++) {
+                if(answerStack.peek() != arr[i]) {
+                    answerStack.push(arr[i]);
+                }
+            }
+
+            int[] answer = new int[answerStack.size()];
+            int idx = answerStack.size();
+            while(!answerStack.isEmpty()) {
+                answer[-- idx] = answerStack.pop();
+            }
+            return answer;
+        }
+    }
 }
