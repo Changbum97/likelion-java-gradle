8e994b54a0548d7e14ea07971530b95832425dba
Changbum97 <chb2005@naver.com>
2022-11-11 10:10:08 +0900

    프로그래머스 이진 변환 반복하기

---
+++ src/main/java/Week6/Day26_1111/RepeatBinaryConvert.java
@@ -0,0 +1,34 @@
+package Week6.Day26_1111;
+
+import java.util.Arrays;
+
+public class RepeatBinaryConvert {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+
+        System.out.println(Arrays.toString(s.solution("110010101001")));
+        System.out.println(Arrays.toString(s.solution("01110")));
+        System.out.println(Arrays.toString(s.solution("1111111")));
+    }
+
+    static class Solution {
+        public int[] solution(String s) {
+            int cnt = 0;
+            int change = 0;
+
+            while(!s.equals("1")) {
+                // 제거할 0의 개수 count
+                for(int i = 0 ; i < s.length() ; i ++) {
+                    if(s.charAt(i) == '0') {
+                        cnt ++;
+                    }
+                }
+                s = s.replace("0", "");
+                s = Integer.toBinaryString(s.length());
+                change ++;
+            }
+
+            return new int[]{change, cnt};
+        }
+    }
+}
