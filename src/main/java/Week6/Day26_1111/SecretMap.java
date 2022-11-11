f09932ca02d4dbc1d114066db25173763342ed4f
Changbum97 <chb2005@naver.com>
2022-11-11 09:41:22 +0900

    프로그래머스 비밀지도

--- src/main/java/Week6/Day26_1111/SecretMap.java
+++ src/main/java/Week6/Day26_1111/SecretMap.java
@@ -1,2 +1,37 @@
-package Week6.Day26_1111;public class SecretMap {
+package Week6.Day26_1111;
+
+public class SecretMap {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        String[] answer = s.solution(5, new int[]{9, 20, 28, 18, 11}, new int[]{30, 1, 21 ,17, 28});
+        answer = s.solution(6, new int[]{46, 33, 33 ,22, 31, 50}, new int[]{27 ,56, 19, 14, 14, 10});
+        for(int i = 0 ; i < answer.length ; i ++) {
+            System.out.println(answer[i]);
+        }
+    }
+
+    static class Solution {
+        public String[] solution(int n, int[] arr1, int[] arr2) {
+            String[] answer = new String[n];
+            String padding = " ".repeat(n);
+
+            for(int i = 0 ; i < n ; i ++) {
+                // or 연산을 통해 하나라도 1이면 1, 둘 다 0이면 0
+                int line = arr1[i] | arr2[i];
+
+                // 한 줄을 binary String으로 전환
+                String binaryString = Integer.toBinaryString(line);
+
+                // padding과 substring을 사용해서 자리수 맞추기
+                answer[i] = (padding + binaryString).substring(binaryString.length());
+
+                // 1 -> #, 0 -> 빈칸 으로 변환
+                answer[i] = answer[i]
+                        .replace('1', '#')
+                        .replace('0', ' ');
+            }
+
+            return answer;
+        }
+    }
 }
