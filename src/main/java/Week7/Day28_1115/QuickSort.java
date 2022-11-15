f343b293f31d2cd247f918de1ee9eec4632f1828
Changbum97 <chb2005@naver.com>
2022-11-15 09:35:58 +0900

    Quick Sort pivot 기준으로 좌우 나누기

--- src/main/java/Week7/Day28_1115/QuickSort.java
+++ src/main/java/Week7/Day28_1115/QuickSort.java
@@ -1,4 +1,34 @@
 package Week7.Day28_1115;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class QuickSort {
+    public static void main(String[] args) {
+        int[] arr = new int[]{20, 18, 5, 19, 5, 25, 40, 50};
+        sort(0, 7, arr);
+    }
+
+    static void sort(int start, int end, int[] arr) {
+        int mid = (start + end) / 2;
+
+        List<Integer> left = new ArrayList<>();
+        List<Integer> right = new ArrayList<>();
+
+        for(int i = start ; i < mid ; i ++) {
+            left.add(arr[i]);
+        }
+
+        for(int i = mid + 1 ; i <= end ; i ++) {
+            right.add(arr[i]);
+        }
+
+        for(int i : left) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+        for(int i : right) {
+            System.out.print(i + " ");
+        }
+    }
 }
