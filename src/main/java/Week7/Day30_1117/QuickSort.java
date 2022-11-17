a29a3152dcdf525a1fa064160bd8e550fc8adb48
Changbum97 <chb2005@naver.com>
2022-11-17 09:28:13 +0900

    Quick Sort 구현

---
+++ src/main/java/Week7/Day30_1117/QuickSort.java
@@ -0,0 +1,37 @@
+package Week7.Day30_1117;
+
+import java.util.Arrays;
+
+public class QuickSort {
+
+    static int[] arr = new int[]{40, 60, 5, 19, 40, 50, 30, 25};
+
+    public static void main(String[] args) {
+        sort(0, arr.length - 1);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    static void sort(int start, int end) {
+        if(start >= end) return;
+
+        int left = start;
+        int right = end;
+        int pivot = arr[(left + right) / 2];
+
+        while(left < right) {
+            while(arr[left] < pivot) left ++;
+            while(arr[right] > pivot) right --;
+
+            if(left <= right) {
+                int temp = arr[left];
+                arr[left] = arr[right];
+                arr[right] = temp;
+                left ++;
+                right --;
+            }
+        }
+
+        sort(start, right);
+        sort(left, end);
+    }
+}
