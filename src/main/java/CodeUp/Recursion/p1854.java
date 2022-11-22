17f81649ef12671f3bfd3407d9693e29dd4422fe
Changbum97 <chb2005@naver.com>
2022-11-22 09:38:46 +0900

    재귀로 각 자리수의 합 구하기

--- src/main/java/CodeUp/Recursion/p1854.java
+++ src/main/java/CodeUp/Recursion/p1854.java
@@ -1,2 +1,15 @@
-package CodeUp.Recursion;public class p1854 {
+package CodeUp.Recursion;
+
+import java.util.Scanner;
+
+public class p1854 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        long n = sc.nextLong();
+        System.out.println(sum(n));
+    }
+    static int sum(long x) {
+        if(x == 0) return 0;
+        return (int)(x % 10) + sum(x / 10);
+    }
 }
