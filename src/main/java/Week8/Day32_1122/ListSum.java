9dbd3a36125f52e84efcb445684d9aa45709ebac
Changbum97 <chb2005@naver.com>
2022-11-22 09:49:00 +0900

    재귀로 List에 있는 모든 값 더하기

---
+++ src/main/java/Week8/Day32_1122/ListSum.java
@@ -0,0 +1,23 @@
+package Week8.Day32_1122;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListSum {
+    public static void main(String[] args) {
+        List<Integer> nums = new ArrayList<>();
+        nums.add(7);
+        nums.add(3);
+        nums.add(2);
+        nums.add(9);
+        System.out.println(sum(nums));
+    }
+    static int sum(List<Integer> nums) {
+        int size = nums.size();
+        if(size == 0) return 0;
+
+        int lastNum = nums.get(size - 1);
+        nums.remove(size - 1);
+        return lastNum + sum(nums);
+    }
+}
