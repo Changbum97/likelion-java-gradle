9d1d3c05a2b8c04c3757583b8d6fc07a6e6c7239
Changbum97 <chb2005@naver.com>
2022-11-23 10:50:03 +0900

    재귀로 nCr 구하기

--- src/main/java/CodeUp/Recursion/p1925.java
+++ src/main/java/CodeUp/Recursion/p1925.java
@@ -1,2 +1,23 @@
-package CodeUp.Recursion;public class p1925 {
+package CodeUp.Recursion;
+
+import java.util.Scanner;
+
+public class p1925 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int r = sc.nextInt();
+
+        if(r < n - r) r = n - r;
+
+        long ans = factorial(r + 1, n);
+        ans /= factorial(1, n - r);
+
+        System.out.println(ans);
+    }
+    static long factorial(int x, int end) {
+        if(x == end) return x;
+
+        return x * factorial(x + 1, end);
+    }
 }
