a16e4f7a9c4d84fae4f4b23741e26d63ea3c7c00
Changbum97 <chb2005@naver.com>
2022-11-28 10:17:27 +0900

    재귀를 활용한 n글자 알파벳 모두 출력

--- src/main/java/Week9/Day36/printAlphabet.java
+++ src/main/java/Week9/Day36/printAlphabet.java
@@ -4,16 +4,20 @@ import java.util.Arrays;
 
 public class printAlphabet {
 
-    public static void main(String[] args) {
-        int n = 2;
+    static int cnt = 0;
 
+    public static void main(String[] args) {
+        int n = 3;
         char[] c = new char[n];
         Arrays.fill(c, 'A');
+
         print(0, c);
+        System.out.println(cnt);
     }
 
     static void print(int nowIdx,  char[] c) {
         if(nowIdx == c.length) {
+            cnt ++;
             System.out.println(Arrays.toString(c));
         }
 
