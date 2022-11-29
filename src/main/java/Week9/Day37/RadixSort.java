c57153e6de64e6848983be1f396b5783a1178f87
Changbum97 <chb2005@naver.com>
2022-11-29 10:19:35 +0900

    중복 제거된 자리수 구하기

--- src/main/java/Week9/Day37/RadixSort.java
+++ src/main/java/Week9/Day37/RadixSort.java
@@ -1,12 +1,18 @@
 package Week9.Day37;
 
 import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 
 public class RadixSort {
     public static void main(String[] args) {
 
-        int[] arr = new int[]{7, 4, 5, 9, 1, 0};
-        int[] radixArr = new int[10];
+        int[] arr = new int[]{7, 4, 5, 9, 1, 0, 23, 333};
+
+        int[] digits = getDigits(arr);
+        System.out.println(Arrays.toString(digits));
+
+        int[] radixArr = new int[500];
         Arrays.fill(radixArr, -1);
 
         for(int num : arr) {
@@ -23,5 +29,21 @@ public class RadixSort {
         }
 
         System.out.println(Arrays.toString(arr));
+    }
+
+    static int[] getDigits(int[] arr) {
+        Set<Integer> set = new HashSet<>();
+        for(int i = 0 ; i < arr.length ; i ++) {
+            if(arr[i] == 0) set.add(1);
+            else set.add((int)(Math.log10(arr[i]) + 1));
+        }
+
+        int[] answer = new int[set.size()];
+        int idx = 0;
+        for(Integer num : set) {
+            answer[idx ++] = num;
+        }
+        Arrays.sort(answer);
+        return answer;
     }
 }
