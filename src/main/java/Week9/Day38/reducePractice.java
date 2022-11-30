b0334cefd61489cc8a961d501a760395e46b55ac
Changbum97 <chb2005@naver.com>
2022-11-30 21:18:42 +0900

    stream, reduce 활용 예제

--- src/main/java/Week9/Day38/reducePractice.java
+++ src/main/java/Week9/Day38/reducePractice.java
@@ -1,2 +1,13 @@
-package Week9.Day38;public class reducePractice {
+package Week9.Day38;
+
+import java.util.Arrays;
+
+public class reducePractice {
+    public static void main(String[] args) {
+        int[] list = {1, 2, 3, 4, 5};
+
+        int sum = Arrays.stream(list)
+                .reduce(0, (a, b) -> a + b); // 재귀와 유사
+        System.out.println("Sum : " + sum);
+    }
 }
