19978677f3613fa548e79927371efce94e42ee74
Changbum97 <chb2005@naver.com>
2022-11-30 11:15:40 +0900

    코드업 두 노드간의 거리

--- src/main/java/CodeUp/Lca/p1936.java
+++ src/main/java/CodeUp/Lca/p1936.java
@@ -1,2 +1,23 @@
-package CodeUp.Lca;public class p1936 {
+package CodeUp.Lca;
+
+import java.util.Scanner;
+
+public class p1936 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        int y = sc.nextInt();
+        System.out.println(lca(x, y));
+    }
+    static int lca(int x, int y) {
+        if(x == y) return 0;
+
+        if(x > y) {
+            return 1 + lca(x / 2, y);
+        } else {
+            return 1 + lca(x, y / 2);
+        }
+    }
+
 }
