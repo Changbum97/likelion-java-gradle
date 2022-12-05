0feba590a126ec63e8e757924375374a4b50ff50
Changbum97 <chb2005@naver.com>
2022-12-05 09:42:47 +0900

    Heap 생성

--- src/main/java/Week10/Day41/MakeHeap.java
+++ src/main/java/Week10/Day41/MakeHeap.java
@@ -1,2 +1,27 @@
-package Week10.Day41;public class MakeHeap {
+package Week10.Day41;
+
+public class MakeHeap {
+    public static void main(String[] args) {
+        int[] arr = new int[]{1, 8, 5, 7, 3, 2, 4, 6, 9};
+        int[] heap = new int[arr.length + 1];
+        heap[1] = arr[0];
+
+        for(int i = 1 ; i < arr.length ; i ++) {
+            heap[i + 1] = arr[i];
+
+            int j = i + 1;
+            while(true) {
+                if(j / 2 == 0 || heap[j / 2] >= heap[j]) break;
+
+                int temp = heap[j];
+                heap[j] = heap[j / 2];
+                heap[j / 2] = temp;
+                j /= 2;
+            }
+        }
+
+        for(int i = 1 ; i <= arr.length ; i ++) {
+            System.out.print(heap[i] + " ");
+        }
+    }
 }
