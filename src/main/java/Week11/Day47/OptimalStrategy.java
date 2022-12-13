d0199e0d687bd02d396876ba6e0e953e1326030c
Changbum97 <chb2005@naver.com>
2022-12-13 09:23:59 +0900

    Optimal Strategy 1단계

--- src/main/java/Week11/Day47/OptimalStrategy.java
+++ src/main/java/Week11/Day47/OptimalStrategy.java
@@ -1,2 +1,32 @@
-package Week11.Day47;public class OptimalStrategy {
+package Week11.Day47;
+
+public class OptimalStrategy {
+    public static void main(String[] args) {
+        int[] arr = new int[]{2, 7, 40, 19};
+        int n = arr.length;
+        Pair[][] dp = new Pair[n][n];
+
+        for(int i = 0 ; i < n ; i ++) {
+            dp[i][i] = new Pair(arr[i], 0);
+        }
+
+        for(int i = 0 ; i < n ; i ++) {
+            for(int j = 0 ; j < n ; j ++) {
+                if(dp[i][j] == null) {
+                    System.out.print("(0, 0)  ");
+                } else {
+                    System.out.printf("(%d, %d)  ", dp[i][j].x, dp[i][j].y);
+                }
+            }
+            System.out.println();
+        }
+    }
+
+    static class Pair {
+        int x, y;
+        Pair(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+    }
 }
